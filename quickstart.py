c2608e0234b6d0206cf36861fa831c45b230c918
BlueJakeLee <brother79a@gmail.com>
2025-02-27 11:00:23 +0900

    Add files via upload

--- quickstart.py
+++ quickstart.py
@@ -11,11 +11,18 @@ siso_config = {
     'login_psw': 'rhksgud!12',
     'year': 2025,
     'month': calendar.APRIL,
-    'weekday': calendar.SUNDAY,
-    'time': "16:00",
+    'weekdays': [{'weekday':calendar.SUNDAY, 'time': "16:00"}, ],
+    # 'dates': [ { 'day': 1, 'time': "16:00" },],
+    'book_info': {
+        "email1": "brother79a",
+        "email2": "nate.com",
+        "userCnt": 14,
+        "purpose": "테스트",
+        "addr": "경기도 시흥시 목감남서35",
+    }
 }
 
 if __name__ == "__main__":
-    siso = Siso(siso_config.get('year'), siso_config.get('month'), siso_config.get('weekday'), siso_config.get('time'))
+    siso = Siso(siso_config)    
     siso.run(siso_config.get('login_id'), siso_config.get('login_psw'))
     print("done")
